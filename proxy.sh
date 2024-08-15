88b4d6b9b766924b6375e2f7420e529bb0a12860
spring-reader <spring_reader@hotmail.com>
2024-08-15 01:46:59 +0000

    proxy: fix https proxy error
    "gnutls_handshake() failed: The TLS connection was non-properly terminated."
    
    Signed-off-by: spring-reader <spring_reader@hotmail.com>

--- proxy.sh
+++ proxy.sh
@@ -6,6 +6,6 @@ CONF_READER_PROXY="${WORK_DIR}/c_proxy.conf"
 if [[ -e $CONF_READER_PROXY ]]; then
 	source $CONF_READER_PROXY
 	export http_proxy="http://${proxy_ip}:${proxy_port}"
-	export https_proxy="https://${proxy_ip}:${proxy_port}"
+	export https_proxy="http://${proxy_ip}:${proxy_port}"
 fi
 
